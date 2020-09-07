e03cb962acb318cc87e4f10691966a3095af9d70
rmanky <rmankaryous12@gmail.com>
2020-09-07 18:11:24 +0000

    improve performance

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,81 +4,90 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
 
-	var	$window = $(window),
-		$body = $('body'),
-		$header = $('#header'),
-		$banner = $('#banner');
+    var $window = $(window),
+        $body = $('body'),
+        $header = $('#header'),
+        $banner = $('#banner'),
+        $banner_video = $('#banner_video');
 
-	// Breakpoints.
-		breakpoints({
-			wide:      ( '1281px',  '1680px' ),
-			normal:    ( '981px',   '1280px' ),
-			narrow:    ( '737px',   '980px'  ),
-			narrower:  ( '737px',   '840px'  ),
-			mobile:    ( '481px',   '736px'  ),
-			mobilep:   ( null,      '480px'  )
-		});
+    $banner_video.width($banner.outerWidth());
+    $banner_video.height($banner.outerHeight());
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+    // Breakpoints.
+    breakpoints({
+        wide: ('1281px', '1680px'),
+        normal: ('981px', '1280px'),
+        narrow: ('737px', '980px'),
+        narrower: ('737px', '840px'),
+        mobile: ('481px', '736px'),
+        mobilep: (null, '480px')
+    });
 
-	// Dropdowns.
-		$('#nav > ul').dropotron({
-			alignment: 'right'
-		});
+    // Play initial animations on page load.
+    $window.on('load', function () {
+        window.setTimeout(function () {
+            $body.removeClass('is-preload');
+        }, 100);
+    });
 
-	// NavPanel.
+    $window.on('resize', function () {
+        $banner_video.width($banner.outerWidth());
+        $banner_video.height($banner.outerHeight());
+    });
 
-		// Button.
-			$(
-				'<div id="navButton">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
+    // Dropdowns.
+    $('#nav > ul').dropotron({
+        alignment: 'right'
+    });
 
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
+    // NavPanel.
 
-	// Header.
-		if (!browser.mobile
-		&&	$header.hasClass('alt')
-		&&	$banner.length > 0) {
+    // Button.
+    $(
+        '<div id="navButton">' +
+        '<a href="#navPanel" class="toggle"></a>' +
+        '</div>'
+    )
+        .appendTo($body);
 
-			$window.on('load', function() {
+    // Panel.
+    $(
+        '<div id="navPanel">' +
+        '<nav>' +
+        $('#nav').navList() +
+        '</nav>' +
+        '</div>'
+    )
+        .appendTo($body)
+        .panel({
+            delay: 500,
+            hideOnClick: true,
+            hideOnSwipe: true,
+            resetScroll: true,
+            resetForms: true,
+            side: 'left',
+            target: $body,
+            visibleClass: 'navPanel-visible'
+        });
 
-				$banner.scrollex({
-					bottom:		$header.outerHeight(),
-					terminate:	function() { $header.removeClass('alt'); },
-					enter:		function() { $header.addClass('alt reveal'); },
-					leave:		function() { $header.removeClass('alt'); }
-				});
+    // Header.
+    if (!browser.mobile
+        && $header.hasClass('alt')
+        && $banner.length > 0) {
 
-			});
+        $window.on('load', function () {
 
-		}
+            $banner.scrollex({
+                bottom: $header.outerHeight(),
+                terminate: function () { $header.removeClass('alt'); },
+                enter: function () { $header.addClass('alt reveal'); },
+                leave: function () { $header.removeClass('alt'); }
+            });
+
+        });
+
+    }
 
 })(jQuery);
